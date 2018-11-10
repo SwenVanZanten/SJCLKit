7e720820c08b19c5100d4fa2c98e210ddfadb34a
Swen van Zanten <info@hdsign.nl>
2018-11-10 14:04:39 +0100

    Update to Swift 4.2

--- SJCLKit/SJCLKit.swift
+++ SJCLKit/SJCLKit.swift
@@ -1,29 +1,29 @@
 import JavaScriptCore
 
 public class SJCL {
-    let encrypt: JSValue
-    let decrypt: JSValue
+    let encrypt: JSValue?
+    let decrypt: JSValue?
     
     public init() {
-        let bundle = NSBundle(forClass: self.dynamicType)
-        let jsPath = bundle.pathForResource("sjcl", ofType: "js")!
+        let bundle = Bundle(for: type(of: self))
+        let jsPath = bundle.path(forResource: "sjcl", ofType: "js")!
         let text = try! String(contentsOfFile: jsPath)
         
         let js = JSContext()
-        js.evaluateScript(text)
+        js?.evaluateScript(text)
         
-        js.evaluateScript("var encrypt = function(pw, data) { return sjcl.encrypt(pw, data); }")
-        js.evaluateScript("var decrypt = function(pw, data) { return sjcl.decrypt(pw, data); }")
+        js?.evaluateScript("var encrypt = function(pw, data) { return sjcl.encrypt(pw, data); }")
+        js?.evaluateScript("var decrypt = function(pw, data) { return sjcl.decrypt(pw, data); }")
         
-        encrypt = js.objectForKeyedSubscript("encrypt")
-        decrypt = js.objectForKeyedSubscript("decrypt")
+        encrypt = js?.objectForKeyedSubscript("encrypt")
+        decrypt = js?.objectForKeyedSubscript("decrypt")
     }
     
     public func encrypt(password: String, data: String)-> String {
-        return self.encrypt.callWithArguments([password, data]).toString()
+        return self.encrypt?.call(withArguments: [password, data]).toString() ?? ""
     }
     
     public func decrypt(password: String, data: String) -> String {
-        return self.decrypt.callWithArguments([password, data]).toString()
+        return self.decrypt?.call(withArguments: [password, data]).toString() ?? ""
     }
 }
