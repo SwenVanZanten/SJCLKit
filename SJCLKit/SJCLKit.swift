5cdad3b9f40a49bca09fb5d688d577908bd3feba
Swen van Zanten <info@hdsign.nl>
2018-11-10 14:18:54 +0100

    Add base64.toBits function

--- SJCLKit/SJCLKit.swift
+++ SJCLKit/SJCLKit.swift
@@ -3,6 +3,7 @@ import JavaScriptCore
 public class SJCL {
     let encrypt: JSValue?
     let decrypt: JSValue?
+    let base64: JSValue?
     
     public init() {
         let bundle = Bundle(for: type(of: self))
@@ -12,18 +13,24 @@ public class SJCL {
         let js = JSContext()
         js?.evaluateScript(text)
         
-        js?.evaluateScript("var encrypt = function(pw, data) { return sjcl.encrypt(pw, data); }")
-        js?.evaluateScript("var decrypt = function(pw, data) { return sjcl.decrypt(pw, data); }")
+        js?.evaluateScript("var encrypt = function(password, plaintext, params) { return sjcl.encrypt(password, plaintext, params); }")
+        js?.evaluateScript("var decrypt = function(password, ciphertext, params) { return sjcl.decrypt(password, ciphertext, params); }")
+        js?.evaluateScript("var toBits = function(encryptingKey) { return sjcl.codec.base64.toBits(encryptingKey); }")
         
         encrypt = js?.objectForKeyedSubscript("encrypt")
         decrypt = js?.objectForKeyedSubscript("decrypt")
+        base64 = js?.objectForKeyedSubscript("toBits")
     }
     
-    public func encrypt(password: String, data: String)-> String {
-        return self.encrypt?.call(withArguments: [password, data]).toString() ?? ""
+    public func encrypt(password: String, plaintext: String, params: Any) -> String {
+        return self.encrypt?.call(withArguments: [password, plaintext, params]).toString() ?? ""
     }
     
-    public func decrypt(password: String, data: String) -> String {
-        return self.decrypt?.call(withArguments: [password, data]).toString() ?? ""
+    public func decrypt(password: String, ciphertext: String, params: Any) -> String {
+        return self.decrypt?.call(withArguments: [password, ciphertext, params]).toString() ?? ""
+    }
+    
+    public func base64ToBits(encryptingKey: String) -> [Int] {
+        return self.base64?.call(withArguments: [encryptingKey])?.toArray() as! [Int]
     }
 }
